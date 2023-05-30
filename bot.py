72454bc7ddb1ae039328c80c334f40d9ce60c462
Nupipun <sonmcdoffin@gmail.com>
2023-05-30 06:33:11 +0200

    archivo bot limpiado clase commands creada
    Creada clase Commands y adaptado el codigo a los nuevos slash commands de discord.py 2.0 mas algunos cambios mas secundarios

--- bot.py
+++ bot.py
@@ -1,101 +1,18 @@
 import os
 import discord
-import psycopg2
 from discord.ext import commands
 from dotenv import load_dotenv
+from commands import Commands
 
 # Leer el token
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='/', intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-
-
-@bot.command()
-async def clear(ctx):
-    response = 'Espero que no estés usando basura, enseguida se te asignarán los puntos.' 
-    await ctx.send(response)
-
-@bot.command()
-async def tabla(ctx):
-    # Establecer la conexión a la base de datos
-    conn = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        database="TRY_NOT_TO_DELTA",
-        user="postgres",
-        password="admin"
-    )
-
-    # Crear una consulta SQL para seleccionar toda la tabla
-    cursor = conn.cursor()
-    query = "SELECT * FROM public.tntd"
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    # Cerrar el cursor y la conexión
-    cursor.close()
-    conn.close()
-
-    # Dividir los resultados en dos partes
-    half = len(results) // 2
-    first_half = results[:half]
-    second_half = results[half:]
-
-    # Crear la descripcióimer n del prmensaje
-    first_half_table = "\n".join([f"{row}" for row in first_half])
-
-    # Crear el primer mensaje
-    first_response = discord.Embed(
-        title="Tabla (Parte 1)",
-        description=first_half_table
-    )
-
-    # Crear la descripción del segundo mensaje
-    second_half_table = "\n".join([f"{row}" for row in second_half])
-
-    # Crear el segundo mensaje
-    second_response = discord.Embed(
-        title="Tabla (Parte 2)",
-        description=second_half_table
-    )
-
-    # Enviar los mensajes en Discord
-    await ctx.send(embed=first_response)
-    await ctx.send(embed=second_response)
-
-@bot.command()
-async def players(ctx):
-    # Establecer la conexión a la base de datos
-    conn = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        database="TRY_NOT_TO_DELTA",
-        user="postgres",
-        password="admin"
-    )
-
-    cursor = conn.cursor()
-    query = "SELECT NOMBRE, PUNTOS  FROM public.players"
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-
-    # Formatear los resultados como un mensaje de Discord
-    player_list = '\n'.join([f'{row[0]} - Puntos: {row[1]}' for row in results])
-    embed = discord.Embed(title="Lista de jugadores", description=player_list, color=discord.Color.blue())
-    await ctx.send(embed=embed)
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+commands = Commands(bot)  # Cargar los comandos de la clase Commands
+# Si es necesario añadir comandos, añadirlos a la clase Commands
+# si tienes alguna duda con mi codigo hablame al md y te lo
+# explico
 
 bot.run(TOKEN)
---
+++ commands.py
@@ -0,0 +1,111 @@
+import discord
+import psycopg2
+
+
+class Commands:
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.conn = None
+        self.start_commands()
+
+    def start_db_connection(self):
+        """Inicializa la conexión a la base de datos"""
+        self.conn = psycopg2.connect(
+                host="localhost",
+                port="5432",
+                database="TRY_NOT_TO_DELTA",
+                user="postgres",
+                password="admin"
+            )
+
+    def start_commands(self):
+        """Inicializa los comandos"""
+        @self.bot.event
+        async def on_ready():
+            print(f'{self.bot.user.name} has connected to Discord!')
+            try:
+                synced = await self.bot.tree.sync()
+                print(f"Synced {len(synced)} command(s)")
+            except Exception as e:
+                print(e)
+
+        @self.bot.tree.command(name="clear")
+        async def clear(interaction: discord.Interaction):
+            msg = (
+                'Espero que no estés usando basura, '
+                'enseguida se te asignarán los puntos.'
+                )
+            await interaction.response.send_message(msg, ephemeral=True)
+
+        @self.bot.tree.command(name="tabla")
+        async def tabla(interaction: discord.Interaction):
+            # FIXME: Estaria bien hacer este codigo mas legible
+            # de algun modo ya que a mi me cuesta entenderlo.
+            # Iniciar conexión a base de datos
+            self.start_db_connection()
+
+            # Crear una consulta SQL para seleccionar toda la tabla
+            cursor = self.conn.cursor()
+            query = "SELECT * FROM public.tntd"
+            cursor.execute(query)
+            results = cursor.fetchall()
+
+            # Cerrar el cursor y la conexión
+            cursor.close()
+            self.conn.close()
+
+            # Dividir los resultados en dos partes
+            half = len(results) // 2
+            first_half = results[:half]
+            second_half = results[half:]
+
+            # Crear la descripcion del primer mensaje
+            first_half_table = "\n".join([f"{row}" for row in first_half])
+
+            # Crear el primer mensaje
+            first_response = discord.Embed(
+                title="Tabla (Parte 1)",
+                description=first_half_table
+            )
+
+            # Crear la descripción del segundo mensaje
+            second_half_table = "\n".join([f"{row}" for row in second_half])
+
+            # Crear el segundo mensaje
+            second_response = discord.Embed(
+                title="Tabla (Parte 2)",
+                description=second_half_table
+            )
+
+            # Enviar los mensajes en Discord
+            await interaction.response.send_message(embed=first_response,
+                                                    ephemeral=True)
+            await interaction.followup.send(embed=second_response,
+                                            ephemeral=True)
+
+        @self.bot.tree.command(name="players")
+        async def players(interaction: discord.Interaction):
+            # Establecer la conexión a la base de datos
+            self.start_db_connection()
+
+            # Consula SQL
+            cursor = self.conn.cursor()
+            query = "SELECT NOMBRE, PUNTOS  FROM public.players"
+            cursor.execute(query)
+            results = cursor.fetchall()
+
+            # Cerrar conexiones
+            cursor.close()
+            self.conn.close()
+
+            # Formatear los resultados como un mensaje de Discord
+            player_list = '\n'.join([
+                f'{row[0]} - Puntos: {row[1]}' for row in results
+                ])
+            embed = discord.Embed(
+                title="Lista de jugadores",
+                description=player_list,
+                color=discord.Color.blue()
+                )
+            await interaction.response.send_message(embed=embed,
+                                                    ephemeral=True)
