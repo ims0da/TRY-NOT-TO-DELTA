41a9d93e5250e5863774e5e602090ceb35a10ff5
ims0da <alejandro.noguera13@gmail.com>
2023-05-30 14:17:43 +0200

    Cambios de nupi.

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 commands = Commands(bot)  # Cargar los comandos de la clase Commands
 # Si es necesario añadir comandos, añadirlos a la clase Commands
 # si tienes alguna duda con mi codigo hablame al md y te lo
