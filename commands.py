730faa24d7a649e4348eb4623c1408ea5265723d
ims0da <alejandro.noguera13@gmail.com>
2023-05-31 20:50:12 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -103,6 +103,10 @@ class Commands:
             results = self.query("SELECT * FROM public.tntd ORDER BY id")
             
             # Obtener los encabezados de las columnas de la tabla
+<<<<<<< Updated upstream
+=======
+            column_headers = []
+>>>>>>> Stashed changes
             # Crear una lista de filas para la tabla
             table_rows = []
             for row in results:
