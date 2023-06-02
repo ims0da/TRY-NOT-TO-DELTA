81894c6b75f057a6614dca76c2fb3f3f780e4332
Nupipun <sonmcdoffin@gmail.com>
2023-06-02 02:45:18 +0200

    cosas mias

--- commands.py
+++ commands.py
@@ -12,6 +12,12 @@ ayuda_comandos = {
 
 
 class Commands:
+    WELCOME_CHANN_ID = 1112809557396299777
+    ROWS_PER_PAGE = 8
+    ID_CANAL_VALIDACION = 1113157312723550339
+    VALIDAR_MAPAS_CANAL_ID = 1113157312723550339
+    MAPAS_RANKEADOS_CHANNEL_ID = 1113929255517171742
+
     def __init__(self, bot) -> None:
         self.bot = bot
         self.conn = None
@@ -71,15 +77,14 @@ class Commands:
 
         @self.bot.event
         async def on_member_join(member):  # No se si funciona porque discord es un mierdolo y me ha dicho q he excedido el numero maximo de requests a mi bot :D
-            welcome_chann_id = 1112809557396299777
-            welcome_chann = self.bot.get_channel(welcome_chann_id)
+            channel = self.bot.get_channel(self.WELCOME_CHANN_ID)
 
             msg = (
                 f'Bienvenido/a {member.mention} a TRY NOT TO DELTA! '
                 f'Esperemos que te lo pases bien por aquí!'
             )
 
-            await welcome_chann.send(msg)
+            await channel.send(msg)
 
         @self.bot.tree.command(name="clear")
         async def clear(interaction: discord.Interaction):
@@ -91,48 +96,67 @@ class Commands:
 
         @self.bot.tree.command(name="tabla")
         async def tabla(interaction: discord.Interaction):
-            # TODO: Estaria guay hacer que si esto llega a 4096
-            # caracteres (maximo de los embeds de discord) se
-            # dividiese en diferentes mensajes. (para poder
-            # darle un formato guay con tabulate igual que en
-            # el comando players)
+            
+            results = self.query("SELECT * FROM public.tntd ORDER BY id")
+            
+            # Obtener los encabezados de las columnas de la tabla
+            # Crear una lista de filas para la tabla
+            table_rows = []
+            for row in results:
+                table_rows.append(row)
 
-            # Hacer consulta SQL
-            results = self.query("SELECT * FROM public.tntd")
+            num_pages = math.ceil(len(table_rows) / self.ROWS_PER_PAGE)
 
-            half = len(results) // 2
-            first_half = results[:half]
-            second_half = results[half:]
+            embed_list = []
+            for page_num in range(num_pages):
+                start_index = page_num * self.ROWS_PER_PAGE
+                end_index = start_index + self.ROWS_PER_PAGE
+                page_rows = table_rows[start_index:end_index]
 
-            # Crear la descripcion del primer mensaje
-            first_half_table = "\n".join([f"{row}" for row in first_half])
+                page_embed = discord.Embed(
+                    title=f"Tabla (Página {page_num + 1}/{num_pages})"
+                    )
 
-            # Crear el primer mensaje
-            first_response = discord.Embed(
-                title="Tabla (Parte 1)",
-                description=first_half_table
-            )
+                for row in page_rows:
+                    page_embed.add_field(name=f"Mapa {row[0]}: ", value=f"[{row[1]} - {row[4]}]({row[3]})", inline=True)
+                    page_embed.add_field(name="Puntos: ", value=f"{row[2]}", inline=True)
+                    page_embed.add_field(name="Requirement: ", value=f"{row[5]}, {row[6]}", inline=True)
+                embed_list.append(page_embed)
 
-            # Crear la descripción del segundo mensaje
-            second_half_table = "\n".join([f"{row}" for row in second_half])
+            await interaction.response.send_message(embed=embed_list[index])
+            canal = interaction.channel_id
 
-            # Crear el segundo mensaje
-            second_response = discord.Embed(
-                title="Tabla (Parte 2)",
-                description=second_half_table
-            )
+            message_history = self.bot.get_channel(canal).history(limit=1)
+            last_message = message_history.ag_frame.f_locals.get('self').last_message
 
-            # Enviar los mensajes en Discord
-            await interaction.response.send_message(embed=first_response,
-                                                    ephemeral=True)
-            await interaction.followup.send(embed=second_response,
-                                            ephemeral=True)
+            await last_message.add_reaction('⬅️')
+            await last_message.add_reaction('➡️')
+
+            def check(reaction, user):
+                return user == interaction.user and str(reaction.emoji) in ['⬅️', '➡️']
+
+            index = 0
+            while True:
+                try:
+                    reaction, _ = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
+
+                    if str(reaction.emoji) == '⬅️':
+                        index -= 1
+                        if index < 0:
+                            index = len(embed_list) - 1
+                    elif str(reaction.emoji) == '➡️':
+                        index += 1
+                        if index >= len(embed_list):
+                            index = 0
+
+                    await last_message.edit(embed=embed_list[index])
+                    await last_message.remove_reaction(reaction, interaction.user)
+                except asyncio.TimeoutError:
+                    break
 
         @self.bot.tree.command(name="players")
         async def players(interaction: discord.Interaction):
-            # Hacer consulta SQL
-            results = self.query("SELECT NOMBRE, PUNTOS  FROM public.players")
-
+            results = self.query("SELECT NOMBRE, PUNTOS FROM public.players WHERE PUNTOS != '0'")
             # Ordenar los resultados de mayor a menor puntos
             sorted_results = sorted(
                 results,
@@ -140,43 +164,41 @@ class Commands:
                 reverse=True
                 )
 
-            # Crear headers de la tabla
             headers = ['Nombre', 'Puntos']
-
-            # Crear la lista en forma de tabla
             formatted_player_list = (
                 tabulate(sorted_results, headers, tablefmt='pipe')
                 )
-
             # Formatear los resultados como un mensaje de Discord
             embed = discord.Embed(
                 title="Lista de jugadores",
                 description=f'```\n{formatted_player_list}\n```',
                 color=discord.Color.blue()
                 )
-
             await interaction.response.send_message(embed=embed)
 
         @self.bot.tree.command(name="requestmap")
         async def requestmap(interaction: discord.Interaction, nombre:str, puntos:int, link:str, diff:str, mods:str, clear:str):
-            id_canal_validacion = 1113157312723550339
-            channel = self.bot.get_channel(id_canal_validacion)
+            
+            channel = self.bot.get_channel(self.ID_CANAL_VALIDACION)
 
             message_content = f"Nombre: {nombre}\nPuntos: {puntos}\nLink: {link}\nDiff: {diff}\nMods: {mods}\nClear: {clear}"
             await channel.send(message_content)
 
-            embed = discord.Embed(
-                            title="Tu mapa ha sido enviado a la cola de validación",
-                            description="Se paciente, que no se cobra por esto :moyai:",
-                            color=discord.Color.green()
-                            )
+            embed = (
+                discord.Embed(
+                    title="Tu mapa ha sido enviado a la cola de validación",
+                    description="Se paciente, "
+                                "que no se cobra por esto :moyai:",
+                    color=discord.Color.green()
+                    )
+            )
             await interaction.response.send_message(embed=embed,
                                                     ephemeral=True)
-            
+
         @self.bot.event
         async def on_raw_reaction_add(payload):
-            canal_id = 1113157312723550339
-            if payload.channel_id == canal_id:
+        
+            if payload.channel_id == self.VALIDAR_MAPAS_CANAL_ID:
                 channel = self.bot.get_channel(payload.channel_id)
                 message = await channel.fetch_message(payload.message_id)
                 content = message.content.split("\n")
@@ -191,6 +213,10 @@ class Commands:
                 self.insert("INSERT INTO public.tntd (nombre, puntos, link, diff, mods, clear) VALUES ('{}', {}, '{}', '{}', '{}', '{}')".format(nombre, puntos, link, diff, mods, clear))
 
                 await message.delete()
+                output_channel = (
+                    self.bot.get_channel(self.MAPAS_RANKEADOS_CHANNEL_ID)
+                    )
+                await output_channel.send(f"Se ha rankeado el mapa **{nombre}-{diff}** con el requerimiento de: **{clear}** y con el valor de **{puntos}** puntos.")
 
         @self.bot.tree.command(name="ayuda")
         async def ayuda(interaction: discord.Interaction):
