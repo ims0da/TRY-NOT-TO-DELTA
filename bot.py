1e88dad9451375d1a41518c2ffd3c5f700fd644a
Nupipun <sonmcdoffin@gmail.com>
2023-06-05 01:59:33 +0200

    he cambiao unas cosas

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 import discord
 from discord.ext import commands
-from commandset import Commandset as Commands
+from commands import Commands
 from dotenv import load_dotenv
 import os
+from commands7k import Commands7k
+from commandset import Commandset
 # Leer el token
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-# Cargar los comandos de la clase Commands
-commands = Commands(bot=bot)
+commands = Commands(bot)
+commands7k_instance = Commands7k(bot)
+commandset_instance = Commandset(bot)
 
 bot.run(TOKEN)
--- commandset.py
+++ commandset.py
@@ -1,101 +1,167 @@
 import discord
-from funciones_tablas import tabla_embed, crear_tabla_players
-from commands7k import Commands7k
+import psycopg2
+from tabulate import tabulate
+import math
 import asyncio
+from constants import *
 
 
-class Commandset(Commands7k):
+class Commandset:
     def __init__(self, bot) -> None:
-        super().__init__(bot)
+        self.bot = bot
+        self.conn = None
         self.start_commands()
 
+    def start_db_connection(self):
+        """Inicializa la conexión a la base de datos"""
+        self.conn = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="TRY_NOT_TO_DELTA",
+            user="postgres",
+            password="admin"
+        )
+
+    # Consulta a base de datos
+    def query(self, string: str):
+        # Iniciar conexión a base de datos
+        self.start_db_connection()
+
+        # Crear una consulta SQL para seleccionar toda la tabla
+        cursor = self.conn.cursor()
+        cursor.execute(string)
+        results = cursor.fetchall()
+
+        # Cerrar el cursor y la conexión
+        cursor.close()
+        self.conn.close()
+
+        # Devolver el resultado
+        return results
+
+    def insert(self, string: str):  # <- este es lo mismo que el metodo query de arriba, solo que cuando quieres insertar, modificar o eliminar, no puedes hacer un fetch.
+        self.start_db_connection()  # <- por eso no hay un return, porque no hay nada que devolver, solo se ejecuta la consulta y ya, es digamos "interno"
+
+        cursor = self.conn.cursor()
+        cursor.execute(string)
+
+        # Si la consulta es una operación de modificación (como INSERT, UPDATE o DELETE),
+        # puedes realizar un commit en la conexión
+        self.conn.commit()
+
+        cursor.close()
+        self.conn.close()
+
     def start_commands(self):
-        super().start_commands()
+        """Inicializa los comandos"""
+        @self.bot.event
+        async def on_ready():
+            print(f'{self.bot.user.name} has connected to Discord!')
+            try:
+                print("Sincronizando comandos...")
+                synced = await self.bot.tree.sync()
+                print(f"Synced {len(synced)} command(s)")
+            except Exception as e:
+                print(e)
+
+        @self.bot.tree.command(name="clearet")
+        async def clearet(interaction: discord.Interaction):
+            msg = (
+                'Espero que no estés usando basura, '
+                'enseguida se te asignarán los puntos.'
+                )
+            await interaction.response.send_message(msg, ephemeral=True)
 
         @self.bot.tree.command(name="tablaet")
         async def tablaet(interaction: discord.Interaction):
             results = self.query("SELECT * FROM public.tntdet ORDER BY id")
+            
+            # Obtener los encabezados de las columnas de la tabla
+            # Crear una lista de filas para la tabla
+            table_rows = []
+            for row in results:
+                table_rows.append(row)
 
-            embed_page_list = tabla_embed(results)
+            num_pages = math.ceil(len(table_rows) / ROWS_PER_PAGE)
+
+            embed_list = []
+            for page_num in range(num_pages):
+                start_index = page_num * ROWS_PER_PAGE
+                end_index = start_index + ROWS_PER_PAGE
+                page_rows = table_rows[start_index:end_index]
+
+                page_embed = discord.Embed(
+                    title=f"Tabla de Etterna (Página {page_num + 1}/{num_pages})"
+                )
+                
+                for row in page_rows:
+                    page_embed.add_field(name=f"Mapa {row[0]}: ", value=f"[{row[1]} - {row[4]}]({row[3]})", inline=True)
+                    page_embed.add_field(name="Puntos: ", value=f"{row[2]}", inline=True)
+                    page_embed.add_field(name="Requirement: ", value=f"{row[5]}, {row[6]}", inline=True)
+                embed_list.append(page_embed)
 
             index = 0
-            await interaction.response.send_message(
-                embed=embed_page_list[index]
-            )
+            message = await interaction.response.send_message(embed=embed_list[index])
             canal = interaction.channel_id
 
             message_history = self.bot.get_channel(canal).history(limit=1)
-            last_message = (
-                message_history.ag_frame.f_locals.get("self").last_message
-            )
+            last_message = message_history.ag_frame.f_locals.get('self').last_message
 
-            await last_message.add_reaction("⬅️")
-            await last_message.add_reaction("➡️")
+            await last_message.add_reaction('⬅️')
+            await last_message.add_reaction('➡️')
 
             def check(reaction, user):
-                return user == (
-                    interaction.user and str(reaction.emoji) in ["⬅️", "➡️"]
-                )
+                return user == interaction.user and str(reaction.emoji) in ['⬅️', '➡️']
 
             while True:
                 try:
-                    reaction, _ = await self.bot.wait_for(
-                        "reaction_add",
-                        timeout=60.0,
-                        check=check
-                    )
+                    reaction, _ = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
 
                     if str(reaction.emoji) == '⬅️':
                         index -= 1
                         if index < 0:
-                            index = len(embed_page_list) - 1
+                            index = len(embed_list) - 1
                     elif str(reaction.emoji) == '➡️':
                         index += 1
-                        if index >= len(embed_page_list):
+                        if index >= len(embed_list):
                             index = 0
 
-                    await last_message.edit(embed=embed_page_list[index])
-                    await last_message.remove_reaction(reaction,
-                                                       interaction.user)
+                    await last_message.edit(embed=embed_list[index])
+                    await last_message.remove_reaction(reaction, interaction.user)
                 except asyncio.TimeoutError:
                     break
 
         @self.bot.tree.command(name="playerset")
-        async def players(interaction: discord.Interaction):
-            results = self.query(
-                "SELECT NOMBRE, PUNTOS FROM "
-                "public.playerset WHERE PUNTOS != '0'"
-            )
-
-            players = crear_tabla_players(results)
+        async def playerset(interaction: discord.Interaction):
+            results = self.query("SELECT NOMBRE, PUNTOS FROM public.playerset WHERE PUNTOS != '0'")
+            # Ordenar los resultados de mayor a menor puntos
+            sorted_results = sorted(
+                results,
+                key=lambda row: row[1],
+                reverse=True
+                )
+            headers = ['Nombre', 'Puntos']
+            formatted_player_list = (
+                tabulate(sorted_results, headers, tablefmt='pipe')
+                )
             embed = discord.Embed(
-                title="Lista de jugadores de etterna",
-                description=f"```\n{players}\n```",
+                title="Lista de jugadores de etterna.",
+                description=f'```\n{formatted_player_list}\n```',
                 color=discord.Color.blue()
-            )
+                )
             await interaction.response.send_message(embed=embed)
 
         @self.bot.tree.command(name="requestmapet")
-        async def requestmapet(interaction: discord.Interaction,
-                               nombre: str, puntos: int, link: str,
-                               diff: str, mods: str, clear: str):
-            channel = self.bot.get_channel(self.VALIDACION_CHANNEL_ID)
-            message_content = (
-                f"Nombre: {nombre}\n"
-                f"Puntos: {puntos}\n"
-                f"Link: {link}\n"
-                f"Diff: {diff}\n"
-                f"Mods: {mods}\n"
-                f"Clear: {clear}"
-                )
+        async def requestmapet(interaction: discord.Interaction, nombre:str, puntos:int, link:str, diff:str, mods:str, clear:str):
+            channel = self.bot.get_channel(ID_CANAL_VALIDACION_ET)
+
+            message_content = f"Nombre: {nombre}\nPuntos: {puntos}\nLink: {link}\nDiff: {diff}\nMods: {mods}\nClear: {clear}"
             await channel.send(message_content)
-            embed = (
-                discord.Embed(
-                    title="Tu mapa ha sido enviado a la cola de validación",
-                    description="Se paciente, "
-                                "que no se cobra por esto :moyai:",
-                    color=discord.Color.green()
-                    )
-            )
+
+            embed = discord.Embed(
+                            title="Tu mapa ha sido enviado a la cola de validación",
+                            description="Se paciente, que no se cobra por esto :moyai:",
+                            color=discord.Color.green()
+                            )
             await interaction.response.send_message(embed=embed,
                                                     ephemeral=True)
