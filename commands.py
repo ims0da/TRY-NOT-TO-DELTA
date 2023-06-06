992fdaefb508699d71b3cb350ec08ec3ebe5466d
Nupipun <101337099+Nupipun@users.noreply.github.com>
2023-06-06 03:06:53 +0200

    Update commands.py
    pequeño bug arreglado

--- commands.py
+++ commands.py
@@ -130,7 +130,7 @@ class Commands:
                 played_map_data = leer_mapas_jugados(player)
 
             if table_map_id in played_map_data:
-                await interaction.followup.send("Ya has jugado ese mapa.")
+                await interaction.followup.send("Ya has jugado ese mapa.", ephemeral=True)
             else:
 
                 try:
