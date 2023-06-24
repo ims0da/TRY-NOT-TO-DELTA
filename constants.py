4d5c926427cd9562e12f520ab4b63c369ce2dcd1
Alejandro Noguera <117599760+ims0da@users.noreply.github.com>
2023-06-25 00:05:59 +0200

    remodelado del codigo de nupi
    y tal

--- constants.py
+++ constants.py
@@ -10,3 +10,11 @@ OUTPUT_CHANNEL_4K_ID = 1113929255517171742
 OUTPUT_CHANNEL_7K_ID = 1114214734015766599
 OUTPUT_CHANNEL_ET_ID = 1114217099775189023
 LOG_CLEAR_CHANNEL_ID = 1115336709840834560
+
+AYUDA_COMANDOS = {
+    "/players": "Muestra la leaderboard con los puntos actuales, también esta /playerset para etterna y /players7k para 7k. ENGLISH: Shows players, pretty self explanatory",
+    "/tabla": "Muestra la base de datos de mapas con el link del mapa, el nombre, la diff name, el mod y el clear, también esta /tablaet para etterna y /tabla7k para 7k. ENGLISH: Shows current maps in the database.",
+    "/clear": "Has hecho un clear a un mapa y requieres de tus puntos",
+    "/ayuda": "Muestra este comando ENGLISH: Shows this command.",
+    "/requestmap": "Requestea un mapa o pack el cual debe ser aprobado por playtesters. /requestmap para 4k, /requestmapet para etterna y /requestmap7k para 7k ENGLISH: Request a map."
+}
